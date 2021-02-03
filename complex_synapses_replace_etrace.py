21b45bf7e4691283f9688f621a7e46e102278f4f
Raymond Chua <ray.r.chua@gmail.com>
2021-02-03 16:42:09 -0500

    change print training log to every 2 episodes

--- complex_synapses_replace_etrace.py
+++ complex_synapses_replace_etrace.py
@@ -355,7 +355,7 @@ for epoch in range(num_epochs):
 		header += ["eps", "cur episode return", "returns", "avg returns", "steps first R", "steps good policy", "cum R"]
 		data += [eps, returnPerEpisode[-1], totalReturn_val, moving_avg_returns, steps_to_first_reward[epoch], steps_to_good_policy[epoch], cumulative_reward]
 
-		if epside_count % 200 == 0: 
+		if epside_count % 2 == 0: 
 			txt_logger.info(
 					"Epoch {} | S {} | Episode {} | D {} | EPS {:.3f} | R {:.3f} | Total R {:.3f} | Avg R {:.3f} | Steps 1st R {}| Steps good P {} | Cum R {}"
 					.format(*data))
