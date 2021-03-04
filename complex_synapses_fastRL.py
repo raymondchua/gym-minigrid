8ecb9d2b3c15785ba57d139345b0af165438dbba
Raymond Chua <ray.r.chua@gmail.com>
2021-03-04 16:14:50 -0500

    added main func

--- complex_synapses_fastRL.py
+++ complex_synapses_fastRL.py
@@ -166,15 +166,15 @@ def compute_SR_td_error(state, next_state, SR_values):
 	return sr_error
 
 
+def main():
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-		"--seed",
-		type=int,
-		help="random seed to generate the environment with",
-		default=0
-	)
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+			"--seed",
+			type=int,
+			help="random seed to generate the environment with",
+			default=0
+		)
 
 	parser.add_argument(
 		"--eps",
@@ -197,148 +197,149 @@ parser.add_argument(
 		default=0.00001
 	)
 
-args = parser.parse_args()
+	args = parser.parse_args()
 
-algo = 'Benna-Fusi_fastRL'
+	algo = 'Benna-Fusi_fastRL'
 
-#create train dir
-date = datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
-default_model_name = f"{algo}_seed{args.seed}_{date}"
+	#create train dir
+	date = datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+	default_model_name = f"{algo}_seed{args.seed}_{date}"
 
-model_name = default_model_name
-model_dir = utils.get_model_dir(model_name)
+	model_name = default_model_name
+	model_dir = utils.get_model_dir(model_name)
 
-# Load loggers and Tensorboard writer
+	# Load loggers and Tensorboard writer
 
-txt_logger = utils.get_txt_logger(model_dir)
-csv_file, csv_logger = utils.get_csv_logger(model_dir)
+	txt_logger = utils.get_txt_logger(model_dir)
+	csv_file, csv_logger = utils.get_csv_logger(model_dir)
 
-# Log command and all script arguments
+	# Log command and all script arguments
 
-txt_logger.info("{}\n".format(" ".join(sys.argv)))
-txt_logger.info("{}\n".format(args))
+	txt_logger.info("{}\n".format(" ".join(sys.argv)))
+	txt_logger.info("{}\n".format(args))
 
-# Set seed for all randomness sources
-utils.seed(args.seed)
+	# Set seed for all randomness sources
+	utils.seed(args.seed)
 
-env = GridWorldEnv(size=grid_size, goal_pos=(0,0))
-txt_logger.info("Environments loaded\n")
+	env = GridWorldEnv(size=grid_size, goal_pos=(0,0))
+	txt_logger.info("Environments loaded\n")
 
-env = GridWorldEnv(size=grid_size)
+	env = GridWorldEnv(size=grid_size)
 
-status = {"num_steps": 0, "update": 0, "num_episodes":0}
-txt_logger.info("Training status loaded\n")
+	status = {"num_steps": 0, "update": 0, "num_episodes":0}
+	txt_logger.info("Training status loaded\n")
 
-Q_u1 = jnp.zeros((grid_size*grid_size, len(env.actions)))
-Q_u2 = jnp.zeros((grid_size*grid_size, len(env.actions)))
-Q_u3 = jnp.zeros((grid_size*grid_size, len(env.actions)))
+	Q_u1 = jnp.zeros((grid_size*grid_size, len(env.actions)))
+	Q_u2 = jnp.zeros((grid_size*grid_size, len(env.actions)))
+	Q_u3 = jnp.zeros((grid_size*grid_size, len(env.actions)))
 
-SR_u1 = np.zeros((grid_size*grid_size, grid_size*grid_size))
-SR_u2 = np.zeros((grid_size*grid_size, grid_size*grid_size))
-SR_u3 = np.zeros((grid_size*grid_size, grid_size*grid_size))
+	SR_u1 = np.zeros((grid_size*grid_size, grid_size*grid_size))
+	SR_u2 = np.zeros((grid_size*grid_size, grid_size*grid_size))
+	SR_u3 = np.zeros((grid_size*grid_size, grid_size*grid_size))
 
-# env1 = GridWorldEnv(size=grid_size, goal_pos=(0,0))
-# env2 = GridWorldEnv(size=grid_size, goal_pos=(grid_size-1,grid_size-1))
-
-
-g_1_2 = args.init_tube #original was 0.00001
-g_2_3 = g_1_2 / 2
-
-C_1 = 1
-C_2 = 2**1
-C_3 = 2**2
-  
-rng =  np.random.default_rng(args.seed)
-
-start_time = time.time()
-totalReturns = []
-returnPerEpisode = [] 
-steps_done = 0
-epside_count = 0
+	# env1 = GridWorldEnv(size=grid_size, goal_pos=(0,0))
+	# env2 = GridWorldEnv(size=grid_size, goal_pos=(grid_size-1,grid_size-1))
 
 
-for epoch in range(num_epochs):
+	g_1_2 = args.init_tube #original was 0.00001
+	g_2_3 = g_1_2 / 2
 
-	
+	C_1 = 1
+	C_2 = 2**1
+	C_3 = 2**2
+	  
+	rng =  np.random.default_rng(args.seed)
 
-	for episode in range(num_episodes):
+	start_time = time.time()
+	totalReturns = []
+	returnPerEpisode = [] 
+	steps_done = 0
+	epside_count = 0
+
+
+	for epoch in range(num_epochs):
+
 		
-		state = env.reset_center()
-		state = getStateID(state)
-		eps_reward = 0
 
-		for time_steps in range(max_steps):
+		for episode in range(num_episodes):
 			
-			if epoch%2==0:
-				action = policy75pLEFT(subkey, len(env.actions))
-			
-			else:
-				action = policy75pRIGHT(subkey, len(env.actions))
+			state = env.reset_center()
+			state = getStateID(state)
+			eps_reward = 0
 
-			next_state, reward, done, info = env.step(map_actions(action))
-			next_state = getStateID(next_state)
-			
-			eps_reward += reward
+			for time_steps in range(max_steps):
+				
+				if epoch%2==0:
+					action = policy75pLEFT(subkey, len(env.actions))
+				
+				else:
+					action = policy75pRIGHT(subkey, len(env.actions))
 
-			#update SR_u1 using SR-td error
-			sr_error = compute_SR_td_error(state, next_state, SR_u1)
-			SR_update_u1 = SR_u1[state,:] + ((lr/C_1) * (sr_error + g_1_2 * (SR_u2[state,:] - SR_u1[state,:])))
-			SR_u1 = jax.ops.index_update(SR_u1, jax.ops.index[state, :], SR_update_u1)
+				next_state, reward, done, info = env.step(map_actions(action))
+				next_state = getStateID(next_state)
+				
+				eps_reward += reward
 
-
-			#update SR_u2 using SR-td error
-			SR_update_u2 = SR_u2[state,:] + ((lr/C_2) * (g_1_2 * (SR_u1[state, :] - SR_u2[state,:]) + \
-							g_2_3*(SR_u3[state, :] - SR_u2[state, :])))
-			SR_u2 = jax.ops.index_update(SR_u2, jax.ops.index[state, :], SR_update_u2)
-
-
-			#update SR_u3 using SR-td error
-			SR_update_u3 = SR_u3[state,:] + ((lr/C_3) * (g_2_3 * (SR_u2[state, :] - SR_u3[state,:])))
-			SR_u3 = jax.ops.index_update(SR_u3, jax.ops.index[state, :], SR_update_u3)
-
-			state = next_state
-
-			if done:
-				returnPerEpisode.append(eps_reward)
-				break
+				#update SR_u1 using SR-td error
+				sr_error = compute_SR_td_error(state, next_state, SR_u1)
+				SR_update_u1 = SR_u1[state,:] + ((lr/C_1) * (sr_error + g_1_2 * (SR_u2[state,:] - SR_u1[state,:])))
+				SR_u1 = jax.ops.index_update(SR_u1, jax.ops.index[state, :], SR_update_u1)
 
 
-		#logging stats
-		duration = int(time.time() - start_time)
-
-		totalReturn_val = jnp.sum(jnp.array(returnPerEpisode))
-		moving_avg_returns = jnp.mean(jnp.array(returnPerEpisode[-10:]))
-
-		header = ["epoch", "steps", "episode", "duration"]
-		data = [epoch, steps_done, epside_count, duration]
-
-		# header += ["eps", "cur episode return", "returns", "avg returns"]
-		# data += [eps, returnPerEpisode[epside_count], totalReturn_val, moving_avg_returns]
-
-		header += ["cur episode return", "returns", "avg returns"]
-		data += [returnPerEpisode[epside_count], totalReturn_val, moving_avg_returns]
-
-		txt_logger.info(
-				"Epoch {} | S {} | Episode {} | D {} | R {:.3f} | Total R {:.3f} | Avg R {:.3f} "
-				.format(*data))
+				#update SR_u2 using SR-td error
+				SR_update_u2 = SR_u2[state,:] + ((lr/C_2) * (g_1_2 * (SR_u1[state, :] - SR_u2[state,:]) + \
+								g_2_3*(SR_u3[state, :] - SR_u2[state, :])))
+				SR_u2 = jax.ops.index_update(SR_u2, jax.ops.index[state, :], SR_update_u2)
 
 
-		if epside_count == 0:
-			csv_logger.writerow(header)
-		csv_logger.writerow(data)
-		csv_file.flush()
+				#update SR_u3 using SR-td error
+				SR_update_u3 = SR_u3[state,:] + ((lr/C_3) * (g_2_3 * (SR_u2[state, :] - SR_u3[state,:])))
+				SR_u3 = jax.ops.index_update(SR_u3, jax.ops.index[state, :], SR_update_u3)
 
-		if epside_count %10 == 0: 
-			filename_u1 = 'SR_u1_'+str(epside_count)+'.npy'
-			filename_u2 = 'SR_u2_'+str(epside_count)+'.npy'
-			filename_u3 = 'SR_u3_'+str(epside_count)+'.npy'
-			jnp.save(filename_u1, SR_u1)
-			jnp.save(filename_u2, SR_u2)
-			jnp.save(filename_u3, SR_u3)
+				state = next_state
 
-		epside_count += 1
+				if done:
+					returnPerEpisode.append(eps_reward)
+					break
 
 
+			#logging stats
+			duration = int(time.time() - start_time)
+
+			totalReturn_val = jnp.sum(jnp.array(returnPerEpisode))
+			moving_avg_returns = jnp.mean(jnp.array(returnPerEpisode[-10:]))
+
+			header = ["epoch", "steps", "episode", "duration"]
+			data = [epoch, steps_done, epside_count, duration]
+
+			# header += ["eps", "cur episode return", "returns", "avg returns"]
+			# data += [eps, returnPerEpisode[epside_count], totalReturn_val, moving_avg_returns]
+
+			header += ["cur episode return", "returns", "avg returns"]
+			data += [returnPerEpisode[epside_count], totalReturn_val, moving_avg_returns]
+
+			txt_logger.info(
+					"Epoch {} | S {} | Episode {} | D {} | R {:.3f} | Total R {:.3f} | Avg R {:.3f} "
+					.format(*data))
+
+
+			if epside_count == 0:
+				csv_logger.writerow(header)
+			csv_logger.writerow(data)
+			csv_file.flush()
+
+			if epside_count %10 == 0: 
+				filename_u1 = 'SR_u1_'+str(epside_count)+'.npy'
+				filename_u2 = 'SR_u2_'+str(epside_count)+'.npy'
+				filename_u3 = 'SR_u3_'+str(epside_count)+'.npy'
+				jnp.save(filename_u1, SR_u1)
+				jnp.save(filename_u2, SR_u2)
+				jnp.save(filename_u3, SR_u3)
+
+			epside_count += 1
+
+if __name__ == "__main__":
+	main()
 
 
 # window.reg_key_handler(key_handler)
